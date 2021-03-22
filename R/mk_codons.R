27cadfdb5b1d18c83c37a4b15b5de762671d8a09
SofieRossen <s175411@student.dtu.dk>
2021-03-22 10:39:45 +0000

    Make codons function

---
+++ R/mk_codons.R
@@ -0,0 +1,12 @@
+#' Pulls out substrings from position s
+#' @param dna input dna string
+#' @param s start postition
+#' @export
+
+mk_codons <- function(dna, s = 1){
+  l = nchar(dna)
+  codons <- substring(dna,
+                      first = seq(from = s, to = l-3+1, by = 3),
+                      last = seq(from = 3+s-1, to = l, by = 3))
+  return(codons)
+}
