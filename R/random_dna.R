7184477a3d9554dc8485278eeda51eea627f08fe
maikajensen <maikajensen@github.com>
2021-04-21 20:07:05 +0000

    Initial project commit

--- R/random_dna.R
+++
@@ -1,8 +0,0 @@
-#' Returns random DNA on l nucleotides
-#' @param l number of wanted nucleotides in the DNA
-#' @export
-random_dna <- function(l){
-  nucleotides <- sample(c("A", "T", "G", "C"), size = l, replace = TRUE)
-  dna = paste0(nucleotides, collapse = "")
-  return(dna)
-}
--- R/random_rna.R
+++
@@ -1,9 +0,0 @@
-#' Creates l random nucleotides from rna
-#' @param l number of chosen nucleotides
-#' @export
-
-random_rna <- function(l){
-  nucleotides <- sample(c("A", "U", "G", "C"), size = l, replace = TRUE)
-  rna = paste0(nucleotides, collapse = "")
-  return(rna)
-}
